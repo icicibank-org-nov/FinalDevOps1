88eb7cb9e716e5929638c137960735a1d211cbb8
krishna.chavan <krishna.chavan@brillio.com>
2022-01-07 13:05:57 +0530

    devops project

---
+++ src/main/resources/com/brillio/AddController.java
@@ -0,0 +1,15 @@
+package com.brillio;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponset;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddController {
+	
+	
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h1>Welcome To DevOps Training</h1>
+</body>
+</html>
